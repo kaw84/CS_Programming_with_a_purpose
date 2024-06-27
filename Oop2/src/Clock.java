ef40c3ea955b26d8c1c12ac5f4501b3245a8d241
kaw84 <128994708+kaw84@users.noreply.github.com>
2024-06-27 18:32:04 +0100

    setting up files

---
+++ Oop2/src/Clock.java
@@ -0,0 +1,23 @@
+public class Clock {
+
+    // Creates a clock whose initial time is h hours and m minutes.
+    public Clock(int h, int m)
+
+    // Creates a clock whose initial time is specified as a string, using the format HH:MM.
+    public Clock(String s)
+
+    // Returns a string representation of this clock, using the format HH:MM.
+    public String toString()
+
+    // Is the time on this clock earlier than the time on that one?
+    public boolean isEarlierThan(Clock that)
+
+    // Adds 1 minute to the time on this clock.
+    public void tic()
+
+    // Adds Δ minutes to the time on this clock.
+    public void toc(int delta)
+
+    // Test client (see below).
+    public static void main(String[] args)
+}
---
+++ Oop2/src/ColorHSB.java
@@ -0,0 +1,18 @@
+public class ColorHSB {
+
+    // Creates a color with hue h, saturation s, and brightness b.
+    public ColorHSB(int h, int s, int b)
+
+    // Returns a string representation of this color, using the format (h, s, b).
+    public String toString()
+
+    // Is this color a shade of gray?
+    public boolean isGrayscale()
+
+    // Returns the squared distance between the two colors.
+    public int distanceSquaredTo(ColorHSB that)
+
+    // Sample client (see below).
+    public static void main(String[] args)
+
+}
